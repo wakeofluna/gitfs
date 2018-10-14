2addac14f838c4750efe855cd6dba4cefe9d562b
Esther Dalhuisen <wakeofluna@astralkey.nl>
2018-10-14 14:06:36 +0200

    Add generic handling of mount options

--- src/gitfs.h
+++ src/gitfs.h
@@ -7,6 +7,27 @@
 
 void print_version(void);
 
+enum mount_option_flag
+{
+	/* Mount options that require an argument */
+	MOUNT_REPOPATH,
+	MOUNT_MOUNTPOINT,
+	MOUNT_OPTION_MAX_VALUE,
+
+	/* Mount options that do not have an argument (flags) */
+	MOUNT_DEBUG,
+	MOUNT_FOREGROUND,
+	MOUNT_READONLY,
+	MOUNT_READWRITE,
+	MOUNT_OPTION_MAX
+};
+
+struct mount_options
+{
+	char *value[MOUNT_OPTION_MAX_VALUE];
+	char flag[MOUNT_OPTION_MAX];
+};
+
 struct gitfs_function
 {
 	const char *description;
--- src/mount.c
+++ src/mount.c
@@ -1,35 +1,117 @@
 #include <assert.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include "gitfs.h"
 
-struct mount_options
+struct mount_flag
 {
-	char *repopath;
-	char *mountpoint;
+	const char *name;
+	const char *description;
+	enum mount_option_flag index;
+};
 
-	char debug;
-	char foreground;
+static const struct mount_flag mount_flags[] = {
+		{ "ro", "Mount the repository readonly (default)", MOUNT_READONLY },
+		{ "rw", "Mount the repository readwrite", MOUNT_READWRITE },
+		{ NULL }
 };
 
 static void mount_help(const char *command)
 {
+	const struct mount_flag *flag;
+	int maxlen = 0;
+
 	print_version();
 	printf("\n%s: %s\n", command, gitfs_mount.description);
 	printf("\nUsage:\n");
 	printf(" %s -h\n", command);
 	printf(" %s <path-to-git-repo> <mountpoint> [-d] [-f] [-o options]\n", command);
+
+	for (flag = mount_flags; flag->name != NULL; ++flag)
+	{
+		int len = strlen(flag->name) + (flag->index < MOUNT_OPTION_MAX_VALUE ? 2 : 0);
+		if (maxlen < len)
+			maxlen = len;
+	}
+
+	printf("\nOptions:\n");
+	printf(" -d %-*s  Enable debugging output (implies -f)\n", maxlen, "");
+	printf(" -f %-*s  Run in foreground\n", maxlen, "");
+	printf("\nMount options:\n");
+
+	for (flag = mount_flags; flag->name != NULL; ++flag)
+	{
+		if (flag->index < MOUNT_OPTION_MAX_VALUE)
+		{
+			int len = strlen(flag->name);
+			printf(" -o %s=X %*s %s\n", flag->name, maxlen - len - 2, "", flag->description);
+		}
+		else
+		{
+			printf(" -o %-*s  %s\n", maxlen, flag->name, flag->description);
+		}
+	}
+
+	printf("\n");
 }
 
 static int parse_option(struct mount_options *options, const char *key, int keylen, const char *value, int valuelen)
 {
+	const struct mount_flag *flag;
+
 	if (keylen == 0)
 		return 0;
 
-	fprintf(stderr, "unknown option: -o %.*s\n", keylen, key);
-	return -1;
+	for (flag = mount_flags; flag->name != NULL; ++flag)
+	{
+		int len = strlen(flag->name);
+		if (len == keylen && memcmp(key, flag->name, len) == 0)
+			break;
+	}
+
+	if (flag->name == NULL)
+	{
+		fprintf(stderr, "unknown option: -o %.*s\n", keylen, key);
+		return -1;
+	}
+
+	if (flag->index < MOUNT_OPTION_MAX_VALUE && valuelen == 0)
+	{
+		fprintf(stderr, "missing required argument to option '%.*s'\n", keylen, key);
+		return -1;
+	}
+
+	if (flag->index >= MOUNT_OPTION_MAX_VALUE && valuelen > 0)
+	{
+		fprintf(stderr, "unexpected argument to option '%.*s': %.*s\n", keylen, key, valuelen, value);
+		return -1;
+	}
+
+	options->flag[flag->index] = 1;
+
+	if (flag->index < MOUNT_OPTION_MAX_VALUE)
+	{
+		free(options->value[flag->index]);
+		options->value[flag->index] = NULL;
+
+		if (valuelen > 0)
+		{
+			char *block = (char*) malloc(valuelen + 1);
+			if (block == NULL)
+			{
+				fprintf(stderr, "out of memory copying option value for '%.*s'\n", keylen, key);
+				return -1;
+			}
+
+			memcpy(block, value, valuelen);
+			block[valuelen] = '\0';
+		}
+	}
+
+	return 0;
 }
 
 static int parse_options(struct mount_options *options, const char *optionstring)
@@ -66,6 +148,33 @@ static int parse_options(struct mount_options *options, const char *optionstring
 	return 0;
 }
 
+static int check_options(const struct mount_options *options, const char *argv0)
+{
+	int retval = 0;
+
+	if (!options->flag[MOUNT_REPOPATH] || !options->value[MOUNT_REPOPATH])
+	{
+		fprintf(stderr, "%s: missing required argument: path-to-git-repo\n", argv0);
+		retval = -1;
+	}
+
+	if (!options->flag[MOUNT_MOUNTPOINT] || !options->value[MOUNT_MOUNTPOINT])
+	{
+		fprintf(stderr, "%s: missing required argument: mountpoint\n", argv0);
+		retval = -1;
+	}
+
+	return retval;
+}
+
+static void free_options(struct mount_options *options)
+{
+	int i;
+
+	for (i = 0; i < MOUNT_OPTION_MAX_VALUE; ++i)
+		free(options->value[i]);
+}
+
 static int mount_main(int argc, char **argv)
 {
 	struct mount_options options = {};
@@ -82,11 +191,10 @@ static int mount_main(int argc, char **argv)
 			switch (opt)
 			{
 				case 'd':
-					options.debug = 1;
-					options.foreground = 1;
-					break;
+					options.flag[MOUNT_DEBUG] = 1;
+					/* no break */
 				case 'f':
-					options.foreground = 1;
+					options.flag[MOUNT_FOREGROUND] = 1;
 					break;
 				case 'h':
 					has_help++;
@@ -116,24 +224,28 @@ static int mount_main(int argc, char **argv)
 		return EXIT_SUCCESS;
 	}
 
-	if (!fixed_args[0])
-		fprintf(stderr, "%s: missing required argument: path-to-git-repo\n", argv[0]);
-
-	if (!fixed_args[1])
+	if (fixed_args[0])
 	{
-		fprintf(stderr, "%s: missing required argument: mountpoint\n", argv[0]);
+		options.value[MOUNT_REPOPATH] = strdup(fixed_args[0]);
+		options.flag[MOUNT_REPOPATH] = 1;
+	}
+
+	if (fixed_args[1])
+	{
+		options.value[MOUNT_MOUNTPOINT] = strdup(fixed_args[1]);
+		options.flag[MOUNT_MOUNTPOINT] = 1;
+	}
+
+	if (check_options(&options, argv[0]) < 0)
+	{
+		free_options(&options);
 		return EXIT_FAILURE;
 	}
 
-	options.repopath = strdup(fixed_args[0]);
-	options.mountpoint = strdup(fixed_args[1]);
+	if (options.flag[MOUNT_DEBUG])
+		printf("Mounting '%s' on '%s'...\n", options.value[MOUNT_REPOPATH], options.value[MOUNT_MOUNTPOINT]);
 
-	if (options.debug)
-		printf("Mounting '%s' on '%s'...\n", options.repopath, options.mountpoint);
-
-	free(options.mountpoint);
-	free(options.repopath);
-
+	free_options(&options);
 	return EXIT_SUCCESS;
 }
 
