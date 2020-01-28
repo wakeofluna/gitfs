de55156568cadf4801376f74e79cb9c0a700921d
Esther Dalhuisen <wakeofluna@astralkey.nl>
2020-01-28 21:26:45 +0100

    Added placeholder text for umount

--- src/umount.cpp
+++ src/umount.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <iostream>
 #include "gitfs.h"
 
 namespace
@@ -6,7 +7,8 @@ namespace
 
 int umount_main(int argc, char **argv)
 {
-	return EXIT_SUCCESS;
+	std::cerr << "Not implemented yet. Use \"fusermount3 -u path\"" << std::endl;
+	return EXIT_FAILURE;
 }
 
 } // namespace
